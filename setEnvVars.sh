1b6c9e4a43d08d6a61b6edaeaadf7425739be646
Drew <drew@openlattice.com>
2021-01-06 18:20:57 -0500

    logging logging logging

--- setEnvVars.sh
+++ setEnvVars.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-export AWS_REGION=us-gov-west-1
+REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}')
+ZONES=$(aws ec2 describe-availability-zones --region "$REGION" | grep ZoneName | awk '{print $2}' | tr -d '"')
+
+export REGION
+export ZONES
 export KOPS_STATE_STORE=s3://kube-test-cluster-config
-export CLUSTER_NAME=kube-test-jupyterhub.k8s.local
+export NAME=kube-test-jupyterhub.k8s.local
 export RELEASE=jupyterhub
 export NAMESPACE=jupyterhub
 export PV_NAME=efs-persist
