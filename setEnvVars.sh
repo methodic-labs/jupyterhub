231c181a4727b59cca87e8511223297c49759f04
Matthew Tamayo-Rios <matthew@openlattice.com>
2021-09-21 21:44:39 -1000

    Fix create cluster and env vars.

--- setEnvVars.sh
+++ setEnvVars.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}')
-ZONES=$(aws ec2 describe-availability-zones --region "$REGION" | grep ZoneName | awk '{print $2}' | tr -d '"')
-
-export REGION
-export ZONES
-export KOPS_STATE_STORE=s3://kube-test-cluster-config
-export NAME=kube-test-jupyterhub.k8s.local
+export KOPS_STATE_STORE=s3://openlattice-jupyterhub-crpc-private
+export KOPS_CLUSTER_NAME=openlattice-jupyterhub.k8s.local
 export RELEASE=jupyterhub
 export NAMESPACE=jupyterhub
 export PV_NAME=efs-persist
